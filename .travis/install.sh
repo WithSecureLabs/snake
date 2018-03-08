6cd40b5931711afb284c8da345279835426dbe0b
Alex Kornitzer <alex.kornitzer@countercept.com>
2018-03-08 21:00:32 +0000

    travis: first go at getting travis to run...

---
+++ .travis/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -ev
+
+# Do the dependencies
+sudo apt-get -y install curl
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get update
+sudo apt-get -y install libyaml-dev mongodb-org nodejs python3-dev python3-pip redis-server libfuzzy-dev ssdeep nginx
+sudo -H pip3 install --upgrade pip setuptools
+
+# Make sure services are running
+sudo systemctl start mongod
+sudo systemctl start redis-server
+
+# Try and install
+sys/install.sh
+
+# Check the install
+systemctl start snake-pit
+systemctl start snake
+systemctl stop nginx
+systemctl start nginx
+
+# Use curl to test
+curl 'http://127.0.0.1:5000'
+curl 'http://127.0.0.1:8000'
